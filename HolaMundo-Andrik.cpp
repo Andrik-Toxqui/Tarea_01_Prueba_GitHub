61a48eea1d89c53b56fcd8356537225af1f792b8
Andrik-Toxqui <79487234+Andrik-Toxqui@users.noreply.github.com>
2021-03-02 21:53:25 -0600

    Agregando programas

---
+++ HolaMundo-Andrik.cpp
@@ -0,0 +1,13 @@
+/* Hernández Toxqui Andrik Tomás
+   Grupo: 2EV4
+   Programa: "HolaMundo_<TU_NOMBRE>.cpp"
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<< "Hola Andrik Toxqui";
+	return 0;
+}
